7d619ad92453069b5be055403335140697ff2db2
Saquib011 <saquibpervez011@gmail.com>
2025-05-31 00:13:55 +0500

    done

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Weather-App-in-React.js/', // << must include slashes and match repo name
 });
