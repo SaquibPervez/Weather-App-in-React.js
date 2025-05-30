fc4facb4efc595ad1bd9f0725936881d04547d1f
Saquib011 <saquibpervez011@gmail.com>
2025-05-31 00:08:36 +0500

    done

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
-)
+);
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Weather-App-in-React.js/', 
+  base: '/Weather-App-in-React.js/', // << must include slashes and match repo name
 });
